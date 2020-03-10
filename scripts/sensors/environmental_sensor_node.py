772137139385be45d4e8fd37c4059e5ea0ada71c
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2020-03-09 17:22:50 -0700

    merged with telemetry display

--- scripts/sensors/environmental_sensor_node.py
+++ scripts/sensors/environmental_sensor_node.py
@@ -1,23 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+
+import environmental_sensor
 import rospy
+from std_msgs.msg import Header
 from sensor_msgs.msg import FluidPressure
 from sensor_msgs.msg import RelativeHumidity
 from sensor_msgs.msg import Temperature
-
-import environmental_sensor
+from cassiopeia.msg import Altitude
 
 
 def talker():
     humidity_pub = rospy.Publisher('cassiopeia/environmental/humidity', RelativeHumidity, queue_size=10)
     temperature_pub = rospy.Publisher('cassiopeia/environmental/temperature', Temperature, queue_size=10)
     pressure_pub = rospy.Publisher('cassiopeia/environmental/pressure', FluidPressure, queue_size=10)
+    altitude_pub = rospy.Publisher('cassiopeia/altitude/', Altitude, queue_size=10)
     rospy.init_node('environmental_sensor', anonymous=False)
     rate = rospy.Rate(2)  # Max rate of BME280 sensor in normal mode in theory is 13.51 Hz
     while not rospy.is_shutdown():
-        humidity_pub.publish(RelativeHumidity(relative_humidity=environmental_sensor.get_humidity()))
-        pressure_pub.publish(FluidPressure(fluid_pressure=environmental_sensor.get_pressure()))
-        temperature_pub.publish(Temperature(temperature=environmental_sensor.get_temperature()))
+        h = Header()
+        h.stamp = rospy.Time.now()
+        humidity_pub.publish(RelativeHumidity(relative_humidity=environmental_sensor.get_humidity(), header=h))
+        pressure_pub.publish(FluidPressure(fluid_pressure=environmental_sensor.get_pressure(), header=h))
+        temperature_pub.publish(Temperature(temperature=environmental_sensor.get_temperature(), header=h))
+        altitude_pub.publish(Altitude(altitude=environmental_sensor.get_altitude(), header=h))
         rate.sleep()
 
 
--- scripts/sensors/imu.py
+++ scripts/sensors/imu.py
@@ -4,7 +4,7 @@ import adafruit_mpu6050
 import board
 import busio
 
-i2c = busio.I2C(board.SCL_1, board.SDA_1)
+i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
 
