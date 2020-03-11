a330a145159bbfd67dad9a68fd53b62008aaa88a
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2020-03-11 07:17:23 -0700

    i2c bus env sensor

--- scripts/sensors/environmental_sensor.py
+++ scripts/sensors/environmental_sensor.py
@@ -8,7 +8,7 @@ import adafruit_bme280
 import board
 import busio
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.SCL_1, board.SDA_1)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
