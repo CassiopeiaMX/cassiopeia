77010706161f8c2dead34f0397f0376bde9eb378
Robert Serrano Kobylyansky <sk.robchik@gmail.com>
2020-03-15 12:32:22 -0700

    dante uwu

--- h265server.sh
+++ h265server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gst-launch-1.0 -e nvarguscamerasrc saturation=0 ! \
-  'video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=60/1' ! \
+  'video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=10/1' ! \
   nvvidconv flip-method=2 ! \
-  nvv4l2h265enc bitrate=160000 ! \
+  nvv4l2h265enc bitrate=80000 ! \
   rtph265pay mtu=1400 ! \
   udpsink host=$BASE_IP port=5000 sync=false async=false
